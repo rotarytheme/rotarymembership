c4cca221aa3bb9eb0bab141f0a61c60f9f51f6f1
rotarytheme <developer@bbrcwebsite.com>
2014-01-31 18:46:22 -0800

    version update

--- rotarymembership.php
+++ rotarymembership.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Rotary Membership
 Description: This is a plugin for Rotary Clubs to Maintain Membership from DacDB. This plugin auto updates from github.
-Version: 2.10
+Version: 2.11
 Author: Merrill M. Mayer
 Author URI: http://www.koolkatwebdesigns.com/
 License: GPL2
